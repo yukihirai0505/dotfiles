c549db7ade3702a370f1f6807c016bd434fb4c82
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-20 13:59:37 +0900

    Remove comment

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-#ln -sf ~/dotfiles/.vim  ~/.vim
-#ln -sf ~/dotfiles/.vimrc ~/.vimrc
-#ln -sf ~/dotfiles/.zshrc ~/.zshrc
-#ln -sf ~/dotfiles/play-1.2.5.6 ~/play-1.2.5.6
-#ln -sf ~/dotfiles/soumu.sh ~/soumu.sh
 set -e
 set -u
 
