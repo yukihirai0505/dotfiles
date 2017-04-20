81e24b38754867904bdbcb7888cfddad884e79b0
平井 優希 <k00068@bz4n-009.cyberagent.co.jp>
2017-04-20 13:49:20 +0900

    Add git files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.vim  ~/.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/play-1.2.5.6 ~/play-1.2.5.6
-ln -sf ~/dotfiles/soumu.sh ~/soumu.sh
+#ln -sf ~/dotfiles/.vim  ~/.vim
+#ln -sf ~/dotfiles/.vimrc ~/.vimrc
+#ln -sf ~/dotfiles/.zshrc ~/.zshrc
+#ln -sf ~/dotfiles/play-1.2.5.6 ~/play-1.2.5.6
+#ln -sf ~/dotfiles/soumu.sh ~/soumu.sh
+set -e
+set -u
+
+setup() {
+    dotfiles=$HOME/dotfiles
+    targets=(".vim" ".vimrc" ".zshrc" "play-1.2.5.6" "soumu.sh" \
+             ".gitconfig" ".gitignore")
+
+    has() {
+        type "$1" > /dev/null 2>&1
+    }
+
+    symlink() {
+        [ -e "$2" ] || ln -sf "$1" "$2"
+    }
+
+    if [ -d "$dotfiles" ]; then
+        (cd "$dotfiles" && git pull)
+    else
+        git clone https://github.com/yukihirai0505/dotfiles "$dotfiles"
+    fi
+
+    for target in ${targets[@]}; do
+      symlink "$dotfiles$target" "$HOME/$target"
+    done
+
+}
+
+setup
