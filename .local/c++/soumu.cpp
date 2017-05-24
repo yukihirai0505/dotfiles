92fa4ea5599fad522213f08a7a627fea781a04a8
Yuki Hirai <yukihirai0505@gmail.com>
2017-05-24 13:58:31 +0900

    Update soumu command

--- .local/c++/soumu.cpp
+++ .local/c++/soumu.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
-  char cmd[] = "rm -Rf $HOME/Downloads/* $HOME/Desktop/* $HOME/.Trash/* $HOME/.Trash/.DS_Store";
+  char cmd[] = "rm -Rf $HOME/Downloads/* $HOME/Desktop/* $HOME/.Trash/* $HOME/.Trash/.*";
   system(cmd);  
 
   return 0;
